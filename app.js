63a125429dedec1933b18362cf01f0c14048ee91
peterlee <1562293224@qq.com>
2017-11-26 23:55:51 +0800

    Feat: first commit
     first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const PORT = 8080
+const http = require('http')
+const fs = require('fs')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+var multipart = require('connect-multiparty') //在处理模块中引入第三方解析模块 
+var multipartMiddleware = multipart()
+var uploadPath = './upload'
+
+var app = express()
+var fileIndex
+
+app.post('/upload', multipartMiddleware, (req, res, next) => {
+  let data = req.body
+  let fileData = req.files.fileContent
+
+  // 上传目录是否存在，不存在则创建
+  if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath)
+  }
+
+  if (!fileIndex || fileIndex < data.fileIndex) {
+    fileIndex = parseInt(data.fileIndex)
+
+    let fileChunk = fs.readFileSync(fileData.path)
+    fs.appendFile('./upload/' + data.fileName, fileChunk)
+  } else {
+    fileIndex = ''
+    fs.renameSync(fileData.path, './upload/' + data.fileName)
+  } 
+
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end()
+})
+
+
+app.get('/', (req, res) => {
+  let form = fs.readFileSync('./index.html', {
+    encoding: 'utf8'
+  })
+
+  res.send(form)
+})
+
+app.listen(PORT)
+console.log('Listening the port: ' + PORT)
